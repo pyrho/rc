f4e11bc4f10d97225b9fa558d11eed052a6f07ba
Damien Rajon <gitlab@25-47.net>
2019-06-18 14:04:24 -0700

    added i3 & polybar

---
+++ polybar/launch.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+## Add this to your wm startup file.
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar
+polybar -c ~/.config/polybar/config.ini top &
+polybar -c ~/.config/polybar/config.ini bottom &
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar -c ~/.config/polybar/config.ini top &
+    MONITOR=$m polybar -c ~/.config/polybar/config.ini bottom &
+  done
+else
+	polybar -c ~/.config/polybar/config.ini bottom &
+fi
