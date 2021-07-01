906e1cd6b5479d7add801bf43bbe55c2fe636959
pyrho <145502+pyrho@users.noreply.github.com>
2021-07-01 13:53:51 +0200

    nvim: fern

--- nvim_lua/lua/pyrho/plugins/conf/fern.lua
+++ nvim_lua/lua/pyrho/plugins/conf/fern.lua
@@ -1,14 +1,23 @@
 local M = {}
 
+function M.mappings()
+  vim.api.nvim_buf_set_keymap(0, 'n', 'u', '<Plug>(fern-action-leave)', {})
+  vim.api.nvim_buf_set_keymap(0, 'n', 'q', '<cmd>bd<CR>', {})
+  -- vim.cmd [[
+  --   nmap u <Plug>(fern-action-leave)
+  --   ]]
+end
+
 function M.config()
-  vim.api.nvim_set_keymap('n', '<Leader>f', ':Fern %:h -reveal=%<CR>',
-                          {noremap = true})
+  vim.api.nvim_set_keymap('n', '<Leader>f', ':Fern %:h -reveal=%:p<CR>',
+                          {noremap = true, silent = true})
   vim.g["fern#renderer"] = "nerdfont"
 
   vim.cmd [[
     augroup MyFernStuff
         au!
         autocmd FileType fern call glyph_palette#apply()
+        autocmd FileType fern :lua require"pyrho.plugins.conf.fern".mappings()
     augroup END
   ]]
 end
