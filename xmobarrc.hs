8fdca325ef1d2f07ff9a3251c234e6b0671db345
pyrho <145502+pyrho@users.noreply.github.com>
2022-01-18 13:07:03 +0100

    xmobar upd

--- xmobarrc.hs
+++ xmobarrc.hs
@@ -4,18 +4,18 @@ Config {
                            , "xft:Font Awesome 5 Brands:pixelsize=12"
                            ]
        , bgColor = "#000000"
-       , fgColor = "#22CCDD"
+       , fgColor = "#ffCCDD"
        , alpha = 200
-       , position = TopW L 85
-       , commands = [ Run Cpu [ "--template" , "\xe0b9 \xf2db <total>%", "-L","0","-H","50","--normal","#1ABC9C","--high","darkred"] 10
+       , position = TopW L 95
+       , commands = [ Run Cpu [ "--template" , "\xe0b9 \xf2db <total>%", "-L","0","-H","50","--normal","#ffccdd","--high","darkred"] 10
                     , Run Memory ["-t","\xe0b9 \xf538 <usedratio>%"] 10
-                    , Run DiskU [("/", "\xe0b9 \xf0a0 <free>")] ["-L", "20", "-H", "60"] 10
+                    , Run DiskU [("/", "\xe0b9 \xf0a0 <free> \xe0b9")] ["-L", "20", "-H", "60"] 10
                     -- , Run Swap [] 10
-                    , Run Date "\xe0b9 \xf017 %a %d.%m. %H:%M \xe0b9" "date" 10
+                    , Run Date "%a %d.%m  %H:%M" "date" 10
                     , Run StdinReader
                     ]
        , sepChar = "%"
        , alignSep = "}{"
-       , template = "%StdinReader% }{ %cpu%  %memory%  %disku%  %date%"
+       , template = " ﬦ  %StdinReader% } %date% { %cpu%  %memory%  %disku%"
        }
 
