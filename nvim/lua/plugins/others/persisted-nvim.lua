6ad4ce6815afbcdc19cc72cb800680501f6bbb65
pyrho <github@25.wf>
2023-10-03 12:08:04 +0200

    nvim: persisted updates

--- nvim/lua/plugins/others/persisted-nvim.lua
+++ nvim/lua/plugins/others/persisted-nvim.lua
@@ -1,13 +1,20 @@
 return {
-      "olimorris/persisted.nvim",
-      cmd = { "SessionLoad", "SessionStart", "SessionStop" },
-      config = function()
-        require("persisted").setup({
-          use_git_branch = true, -- create session files based on the branch of the git enabled repository
-          follow_cwd = true, -- change session file name to match current working directory if it changes
-          autoload = false, -- automatically load the session for the cwd on Neovim startup
-          allowed_dirs = {"~/code/caribou/main-repo", "~/code/caribou/monorepo"}, -- table of dirs that the plugin will auto-save and auto-load from
-          branch_separator = "@@"
-        })
-      end
-    }
+  "olimorris/persisted.nvim",
+  cmd = {"SessionLoad", "SessionStart", "SessionStop"},
+  config = function()
+    require("persisted").setup({
+      autosave = true,
+      should_autosave = function()
+        -- do not autosave if the alpha dashboard is the current filetype
+        if vim.bo.filetype == "dashboard" then return false end
+        return true
+      end,
+      use_git_branch = true, -- create session files based on the branch of the git enabled repository
+      follow_cwd = true, -- change session file name to match current working directory if it changes
+      autoload = false, -- automatically load the session for the cwd on Neovim startup
+      -- 2023-09-27 Not sure why I did this, but I'm disabling it for now
+      --allowed_dirs = {"~/code/caribou/main-repo", "~/code/caribou/monorepo"}, -- table of dirs that the plugin will auto-save and auto-load from
+      branch_separator = "@@"
+    })
+  end
+}
