71dbb8a6b0b1692db309c417e57e3e573fd828d2
pyrho <github@25.wf>
2023-11-14 15:35:45 +0100

    updates

--- nvim/lua/plugins/others/zk-nvim.lua
+++ nvim/lua/plugins/others/zk-nvim.lua
@@ -5,7 +5,7 @@ return {
     require("zk").setup({
       -- can be "telescope", "fzf" or "select" (`vim.ui.select`)
       -- it's recommended to use "telescope" or "fzf"
-      picker = "telescope",
+      picker = "select",
 
       lsp = {
         -- `config` is passed to `vim.lsp.start_client(config)`
