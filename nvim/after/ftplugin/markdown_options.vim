d6e3a7d5393704adac7d4f0c82c4a4516664fae2
Damien Rajon <github@25.wf>
2025-05-21 12:37:26 +0200

    nvim(plugin): disable lexima on md files

--- nvim/after/ftplugin/markdown_options.vim
+++ nvim/after/ftplugin/markdown_options.vim
@@ -1,1 +1,5 @@
 setlocal textwidth=80
+
+" Disabling Lexima (auto close pairs) for markdown because it's annoying
+" when writing TODOs ([]).
+let b:lexima_disabled = 1
--- nvim/lua/plugins/others/lexima-vim.lua
+++ nvim/lua/plugins/others/lexima-vim.lua
@@ -1,6 +1,8 @@
 -- pairs autoclosing
 return {
-    event = "InsertEnter",
-  "cohama/lexima.vim",
-  config = function() vim.g.lexima_enable_basic_rules = 1 end
+	event = "VeryLazy",
+	"cohama/lexima.vim",
+	config = function()
+		vim.g.lexima_enable_basic_rules = 1
+	end,
 }
