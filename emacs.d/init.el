8a7303dca55b394d5cdfcd63c040775240d22793
Damien Rajon <github@25.wf>
2025-06-05 00:27:34 +0200

    emacs: initial commit

---
+++ emacs.d/init.el
@@ -0,0 +1,71 @@
+;;; init.el --- Emacs init file
+;;  Author: Ian Y.E. Pan
+;;; Commentary:
+;;; A lightweight Emacs config containing only the essentials: shipped with a custom theme!
+;;; Code:
+(defvar file-name-handler-alist-original file-name-handler-alist)
+
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 0.6
+      file-name-handler-alist nil
+      site-run-file nil)
+
+(defvar ian/gc-cons-threshold 100000000)
+
+(add-hook 'emacs-startup-hook ; hook run after loading init files
+          (lambda ()
+            (setq gc-cons-threshold ian/gc-cons-threshold
+                  gc-cons-percentage 0.1
+                  file-name-handler-alist file-name-handler-alist-original)))
+
+(add-hook 'minibuffer-setup-hook (lambda ()
+                                   (setq gc-cons-threshold (* ian/gc-cons-threshold 2))))
+(add-hook 'minibuffer-exit-hook (lambda ()
+                                  (garbage-collect)
+                                  (setq gc-cons-threshold ian/gc-cons-threshold)))
+
+(require 'package)
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
+(setq package-enable-at-startup nil)
+(package-initialize)
+
+;; workaround bug in Emacs 26.2
+(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+
+
+;; Straight.el -- Package manager
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+
+(use-package straight
+  :custom
+  (straight-use-package-by-default t))
+
+;; Tangle config.org if it's newer than config.el and load it
+(let ((config-org (expand-file-name "config.org" user-emacs-directory))
+      (config-el (expand-file-name "config.el" user-emacs-directory)))
+  (when (or (not (file-exists-p config-el))
+            (file-newer-than-file-p config-org config-el))
+    (require 'org)
+    (org-babel-tangle-file config-org))
+  (load-file config-el))
+
+(provide 'init)
+;;; init.el ends here
